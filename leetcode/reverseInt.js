a280c627918281ab266ba2d1443a8512ed0d0f07
Tyler Weng <tylercweng@gmail.com>
2017-05-20 18:18:41 -0700

    add reverseInt

---
+++ leetcode/reverseInt.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverseInt = function(x) {
+    let isNeg = false;
+    if (x < 0) {
+        isNeg = true;
+        x *= -1;
+    }
+    let reversed = 0;
+    while (x > 0) {
+        let tail = x % 10;
+        reversed = reversed * 10 + tail;
+        x = Math.floor(x / 10);
+    }
+    return isNeg ? - reversed : reversed;
+};
