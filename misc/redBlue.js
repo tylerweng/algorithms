3c5ffda9ecd1b8f38b509c9b86e35766aba79bf0
Tyler Weng <tylercweng@gmail.com>
2017-05-26 18:04:03 -0700

    add redBlue

--- misc/redBlue.js
+++ misc/redBlue.js
@@ -1,20 +1,66 @@
-const match = (str, pat, map = {}) => {
-  if (str.length === 0 && pat.length === 0) return true;
-  if (str.length === 0 || pat.length === 0) return false;
-  if (pat[0] in map) {
-    let matchingChars = map[pat[0]];
-    let len = matchingChars.length;
-    if (matchingChars === str.slice(0, len)) {
-      return match(str.slice(len), pat.slice(1), map);
+function wordpattern(pattern, input, map = {}) {
+    if (pattern.length === 0 && input.length === 0) return 1;
+    if (pattern.length === 0 || input.length === 0) return 0;
+    if (pattern[0] in map) {
+        let chars = map[pattern[0]];
+        let len = chars.length;
+        if (chars === input.slice(0, len)) {
+            return wordpattern(pattern.slice(1), input.slice(len), map);
+        } else {
+            delete map[pattern[0]];
+        }
     } else {
-      delete map[pat[0]];
-      return false;
+        for (let i = 1; i <= input.length - pattern.length; i++) {
+            map[pattern[0]] = input.slice(0, i);
+            if (wordpattern(pattern.slice(1), input.slice(i), map)) {
+              console.log(map)
+              return 1;
+            }
+        }
     }
-  } else {
-    for (let i = 0; i <= str.length - pat.length; i++) {
-      map[pat[0]] = str.slice(0, i);
-      if (match(str.slice(i), pat.slice(1), map)) return true;
+    console.log(map);
+    return 0;
+}
+
+function wordpattern(pattern, input, map = {}) {
+  if (pattern.length === 0 && input.length === 0) return 1;
+  for (let i = 0; i < input.length; i++) {
+    let str = input.slice(0, i + 1);
+    let cached = map[pattern[0]];
+    if (cached) {
+      if (cached[1] === pattern.length) map[pattern[0]][0] = str;
+      if (str !== cached[0]) continue;
+    } else {
+      map[pattern[0]] = [str, pattern.length];
+    }
+    if (wordpattern(pattern.slice(1), input.slice(i + 1), map)) return 1;
+  }
+  return 0;
+}
+
+function wordpattern(pattern, input, map = {}) {
+  if (pattern.length === 0 && input.length === 0) return 1;
+  for (let i = 0; i < input.length; i++) {
+    let str = input.slice(0, i + 1);
+    let cached = map[pattern[0]];
+    if (cached) {
+      if (cached[1] === pattern.length) map[pattern[0]][0] = str;
+      if (str !== cached[0]) continue;
+    } else {
+      map[pattern[0]] = [str, pattern.length];
+    }
+    if (wordpattern(pattern.slice(1), input.slice(i + 1), map)) {
+      let vals = Object.values(map);
+      let uniqVals = vals.filter((v, i, a) => a.indexOf(v) === i);
+      return vals.length === uniqVals.length;
     }
   }
-  return false;
-};
+  return 0;
+}
+
+
+
+console.log(match('xyzabsxzyabs', 'aabb') === false);
+console.log(match('redblueredblue', 'abba') === true);
+console.log(match('asdasdasdasd', 'aaaa') === true);
+console.log(match('xyzabcxzyabc', 'aabb') === false);
