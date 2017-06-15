853e151e3e98e60a75e47bb02ef4150394a0b9ca
Tyler Weng <tylercweng@gmail.com>
2017-06-14 18:46:27 -0700

    update validParentheses

--- leetcode/validParentheses.js
+++ leetcode/validParentheses.js
@@ -4,7 +4,7 @@ const map = {
     "{" : "}"
 };
 
-const validParentheses2 = str => {
+const validParentheses = str => {
     const stack = [];
     for (let i = 0; i < str.length; i++) {
         const char = str[i];
