c2c34c8905c1ec920dc7e52d3e3c9e02b3532d08
Tyler Weng <tylercweng@gmail.com>
2017-06-14 17:28:27 -0700

    add traverseRight

--- leetcode/traverseRight.js
+++ leetcode/traverseRight.js
@@ -11,6 +11,7 @@ const traverseRight = node => {
     let encounteredLevels = new Set();
     queue.push({level, node});
     while (queue.length > 0) {
+      console.log("queue", queue);
         let obj = queue.shift();
         let node = obj.node;
         level = obj.level;
@@ -18,7 +19,7 @@ const traverseRight = node => {
             encounteredLevels.add(level);
             res.push(node.data);
         }
-        if (node.right !== null) queue.unshift({level: level + 1, node: node.right});
+        if (node.right !== null) queue.push({level: level + 1, node: node.right});
         if (node.left !== null) queue.push({level: level + 1, node: node.left});
     }
     return res;
@@ -28,8 +29,11 @@ let node = new BTNode(5);
 node.right = new BTNode(6);
 node.right.right = new BTNode(8);
 node.right.left = new BTNode(10);
+node.right.left.right = new BTNode(11);
 node.left = new BTNode(12);
 node.left.left = new BTNode(13);
 node.left.left.right = new BTNode(14);
+node.left.left.right.right = new BTNode(15);
+node.left.left.right.right.left = new BTNode(100);
 
-traverseRight(node) // [5, 6, 8, 14]
+traverseRight(node) // [5, 6, 8, 11, 15, 100]
