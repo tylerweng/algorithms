7a6ad36c17f0288c09fae64a0f924e281a1c450b
Tyler Weng <tylercweng@gmail.com>
2018-02-01 16:57:55 -0800

    Add binaryTree

---
+++ leetcode/binaryTree.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    List<Integer> inOrderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+        }
+        return list;
+    }
+
+    int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) break;
+            root = root.right;
+        }
+        return root.val;
+    }
+
+    boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        TreeNode prev = null;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (prev != null && root.val <= prev.val) return false;
+            prev = root;
+            root = root.right;
+        }
+        return true;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
