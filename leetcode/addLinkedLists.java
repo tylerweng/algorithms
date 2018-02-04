186a15e66d0d874b2523e3b42284639e0d6dbe12
Tyler Weng <tylercweng@gmail.com>
2018-02-03 16:24:05 -0800

    addLinkedLists

--- leetcode/addLinkedLists.java
+++ leetcode/addLinkedLists.java
@@ -8,21 +8,21 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode prev = new ListNode(0);
-        ListNode head = prev;
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
         int rem = 0;
         while (l1 != null || l2 != null || rem != 0) {
-            ListNode curr = new ListNode(0);
+            ListNode next = new ListNode(0);
             int l1v = (l1 == null ? 0 : l1.val);
             int l2v = (l2 == null ? 0 : l2.val);
             int sum = l1v + l2v + rem;
-            curr.val = sum % 10;
+            next.val = sum % 10;
             rem = sum / 10;
-            prev.next = curr;
-            prev = curr;
+            head.next = next;
+            head = next;
             if (l1 != null) l1 = l1.next;
             if (l2 != null) l2 = l2.next;
         }
-        return head.next;
+        return dummy.next;
     }
 }
