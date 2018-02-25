c2f2e2e71e44d50535370a9d428a4b783eefbd69
Tyler Weng <tylercweng@gmail.com>
2018-02-25 13:06:36 -0800

    Add firstBadVersion w/o long convert

--- leetcode/firstBadVersion.java
+++ leetcode/firstBadVersion.java
@@ -22,3 +22,21 @@ public class Solution extends VersionControl {
         return helper(1L, Long.valueOf(n));
     }
 }
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while (start < end) {
+            int mid = start + ((end - start) / 2);
+            if (!isBadVersion(mid)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
