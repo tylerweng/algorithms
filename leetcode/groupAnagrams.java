c6368fd3cc61c1ba96488b244b3d3bc03b35fe9e
Tyler Weng <tylercweng@gmail.com>
2018-03-19 21:17:33 -0700

    Add groupAnagrams

---
+++ leetcode/groupAnagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String s = String.valueOf(chars);
+            if (!map.containsKey(s)) {
+                map.put(s, new ArrayList<>());
+            }
+            map.get(s).add(str);
+        }
+        return new ArrayList(map.values());
+    }
+}
