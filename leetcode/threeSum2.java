e46af2e010dc4eaab54f1044fed101f998f72179
Tyler Weng <tylercweng@gmail.com>
2018-03-22 18:15:33 -0700

    Add threeSum2.java

---
+++ leetcode/threeSum2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int low = i+1, high = nums.length - 1, target = 0 - nums[i];
+                while (low < high) {
+                    if (nums[low] + nums[high] == target) {
+                        res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while (low < high && nums[low] == nums[low+1]) low++;
+                        while (low < high && nums[high] == nums[high-1]) high--;
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] < target) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
