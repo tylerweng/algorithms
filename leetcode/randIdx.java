ec4ed43715bfeec0d588e8eb2668bd457c031e40
Tyler Weng <tylercweng@gmail.com>
2018-04-06 15:54:49 -0700

    randIdx

--- leetcode/randIdx.java
+++ leetcode/randIdx.java
@@ -19,6 +19,30 @@ class Solution {
     }
 }
 
+class Solution {
+
+    int[] nums;
+    Random rnd;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rnd = new Random();
+    }
+
+    public int pick(int target) {
+        int result = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target)
+                continue;
+            if (rnd.nextInt(++count) == 0)
+                result = i;
+        }
+
+        return result;
+    }
+}
+
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(nums);
