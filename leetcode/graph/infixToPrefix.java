65a35a10c726471c30bbe3675556ae3435c52743
Tyler Weng <tylercweng@gmail.com>
2018-08-28 19:32:41 -0700

    postfix as well

--- leetcode/graph/infixToPrefix.java
+++ leetcode/graph/infixToPrefix.java
@@ -8,6 +8,7 @@ class Main {
         else if (c == '*' || c == '/') return 2;
         else return 0;
     }
+
     public static String infixToPrefix(String infix) {
         Stack<Character> opStack = new Stack<>();
         Stack<Character> output = new Stack<>();
@@ -41,8 +42,50 @@ class Main {
 
         return sb.toString();
     }
+
+    public static String infixToPostfix(String infix) {
+        Stack<Character> opStack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        char[] chars = infix.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == '(') {
+                opStack.push(c);
+            } else if (Character.isDigit(c) || Character.isAlphabetic(c))  {
+                sb.append(c);
+            } else if (c == ')') {
+                while (opStack.peek() != '(') {
+                    sb.append(opStack.pop());
+                }
+                opStack.pop();
+            } else if (OPERATORS.contains(c)) {
+                while (!opStack.isEmpty() && priority(opStack.peek()) >= priority(c)) {
+                    sb.append(opStack.pop());
+                }
+                opStack.push(c);
+            }
+        }
+
+        while (!opStack.isEmpty()) {
+            sb.append(opStack.pop());
+        }
+
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
+        System.out.println("***PREFIX***");
+        System.out.println(infixToPrefix("A*B+C*D"));
         System.out.println(infixToPrefix("7+2*(3-5)"));
         System.out.println(infixToPrefix("4+2*3"));
+        System.out.println(infixToPrefix("(4+2)*3"));
+        System.out.println(infixToPrefix("1+2+3+4"));
+        System.out.println("***POSTFIX***");
+        System.out.println(infixToPostfix("A*B+C*D"));
+        System.out.println(infixToPostfix("7+2*(3-5)"));
+        System.out.println(infixToPostfix("4+2*3"));
+        System.out.println(infixToPostfix("(4+2)*3"));
+        System.out.println(infixToPostfix("1+2+3+4"));
+
     }
 }
