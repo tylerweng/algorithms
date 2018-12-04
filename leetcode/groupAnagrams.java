cf2ebb8f15f652f37627a891221d5a6bdb4b1302
Tyler Weng <tylercweng@gmail.com>
2018-12-03 19:55:43 -0800

    update groupAnagrams

--- leetcode/groupAnagrams.java
+++ leetcode/groupAnagrams.java
@@ -2,9 +2,8 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String s = String.valueOf(chars);
+            // String s = sortString(str);
+            String s = compactString(str);
             if (!map.containsKey(s)) {
                 map.put(s, new ArrayList<>());
             }
@@ -12,4 +11,25 @@ class Solution {
         }
         return new ArrayList(map.values());
     }
+
+    public String compactString(String str) {
+        int[] chars = new int[26];
+        for (char c : str.toCharArray()) {
+            chars[c - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] > 0) {
+                sb.append((char)(i + 'a'));
+                sb.append(chars[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    // public String sortString(String str) {
+    //     char[] chars = str.toCharArray();
+    //     Arrays.sort(chars);
+    //     return new String(chars);
+    // }
 }
