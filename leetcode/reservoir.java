9d82d6ff873f3be1f2d46ea1ffcabc22ed67d4d6
Tyler Weng <tylercweng@gmail.com>
2018-12-05 21:23:41 -0800

    Better reservoir example

--- leetcode/reservoir.java
+++ leetcode/reservoir.java
@@ -8,13 +8,30 @@ class Main {
      * second value has a (1 - 2/6) * (2 / 5) + (2/6) * (1/5) chance of being set to 1
      * == (4/6) * (2/5) + (2/6) * (1/5)
      * == (8/30) + (2/30) == (10/30) == 1/3
+     * this continues ad nausea giving each pos in our matrix a 1/3 chance og being set to 1
      */
 
+
     public static void main(String[] args) {
-        int[][] matrix = new int[2][3];
+        int m = 2;
+        int n = 3;
+        int[][] tally = new int[m][n];
+        int[][] matrix = new int[m][n];
         int k = 2;
-        fillAtRandom(matrix, k);
-        System.out.println(Arrays.deepToString(matrix));
+        int numSamples = 600;
+        for (int i = 0; i < numSamples; i++) {
+            fillAtRandom(matrix, k);
+            for (int x = 0; x < m; x++) {
+                for (int y = 0; y < n; y++) {
+                    if (matrix[x][y] == 1) tally[x][y]++;
+                }
+            }
+            for (int z = 0; z < m; z++) {
+                Arrays.fill(matrix[z], 0);
+            }
+        }
+        System.out.println(String.format("Tally after %d samples", numSamples));
+        System.out.println(Arrays.deepToString(tally));
     }
     static void fillAtRandom(int[][] matrix, int k) {
         int m = matrix.length;
